b669a5bbe34d504fd27309125749a1496dfb7787
Joonhaeng Lee <joonhaeng.lee@navercorp.com>
2019-11-16 18:16:13 +0900

    Add some code to DemoService.java which test with json-mock server

--- src/main/java/com/linecorp/devday/handson/demo/service/DemoService.java
+++ src/main/java/com/linecorp/devday/handson/demo/service/DemoService.java
@@ -1,14 +1,35 @@
 package com.linecorp.devday.handson.demo.service;
 
+import com.linecorp.devday.handson.demo.model.DemoModel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
+
+import java.util.Random;
+import java.util.UUID;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class DemoService {
+
+    private final WebClient webClient;
+
+    public Mono<DemoModel> getRandomDemoModel() {
+
+        Random random = new Random();
+
+        return webClient.get()
+                .uri((UriComponentsBuilder
+                        .fromHttpUrl("https://postman-echo.com/get?randomNumber={randomNumber}&uuid={uuid}")
+                        .build(random.nextInt(10), UUID.randomUUID())))
+                .retrieve()
+                .bodyToMono(DemoModel.class);
+
+    }
 
     public Mono<String> getHello() {
 
