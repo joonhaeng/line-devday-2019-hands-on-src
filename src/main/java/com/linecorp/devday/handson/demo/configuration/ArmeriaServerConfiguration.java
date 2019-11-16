e143b500fdd31cf6dd38b1d7eb423a3a61bda6e6
Joonhaeng Lee <joonhaeng.lee@navercorp.com>
2019-11-16 18:00:17 +0900

    add GrpcService.builder() in ArmeriaServerConfiguration

--- src/main/java/com/linecorp/devday/handson/demo/configuration/ArmeriaServerConfiguration.java
+++ src/main/java/com/linecorp/devday/handson/demo/configuration/ArmeriaServerConfiguration.java
@@ -1,13 +1,19 @@
 package com.linecorp.devday.handson.demo.configuration;
 
+import com.linecorp.armeria.server.grpc.GrpcService;
 import com.linecorp.armeria.server.logging.AccessLogWriter;
 import com.linecorp.armeria.server.logging.LoggingService;
 import com.linecorp.armeria.spring.ArmeriaServerConfigurator;
+import com.linecorp.devday.handson.demo.service.DemoGrpcService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class ArmeriaServerConfiguration {
+
+    private final DemoGrpcService demoGrpcService;
 
     @Bean
     public ArmeriaServerConfigurator armeriaServerConfigurator() {
@@ -16,6 +22,8 @@ public class ArmeriaServerConfiguration {
 
             serverBuilder.decorator(LoggingService.newDecorator());
             serverBuilder.accessLogWriter(AccessLogWriter.combined(), false);
+
+            serverBuilder.service(GrpcService.builder().addService(demoGrpcService).build());
 
         };
 
